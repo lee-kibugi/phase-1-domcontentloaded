586f30ed61a70b8fc55458ba4e8987193959eb4b
linda wanjiku <lindashykooh@gmail.com>
2023-10-04 03:30:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // This code will run when the DOM content has loaded
+    const textElement = document.getElementById("text");
+    textElement.textContent = "This is really cool!";
+  });
